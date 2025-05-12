af39a5f2d91025a251110b56c7ebf10968531c96
arwidmark <arwidmark@gmail.com>
2025-05-12 11:17:42 -0500

    M5

--- Module 5/UpdateMicrosoftGraphSDK.ps1
+++ Module 5/UpdateMicrosoftGraphSDK.ps1
@@ -1,3 +1,18 @@
+# Remove any depending modules
+$Modules = Get-Module Microsoft.Entra* -ListAvailable | Select-Object Name -Unique
+Foreach ($Module in $Modules)
+{
+    $ModuleName = $Module.Name
+    $Versions = Get-Module $ModuleName -ListAvailable
+    Foreach ($Version in $Versions)
+    {
+        $ModuleVersion = $Version.Version
+        Write-Host "Uninstall-Module $ModuleName $ModuleVersion"
+        Uninstall-Module $ModuleName -RequiredVersion $ModuleVersion
+    }
+}
+
+# Remove existing Microsoft.Graph modules
 $Modules = Get-Module Microsoft.Graph* -ListAvailable | Where {$_.Name -ne "Microsoft.Graph.Authentication"} | Select-Object Name -Unique
 Foreach ($Module in $Modules)
 {
@@ -10,7 +25,8 @@ Foreach ($Module in $Modules)
         Uninstall-Module $ModuleName -RequiredVersion $ModuleVersion
     }
 }
-#Uninstall Microsoft.Graph.Authentication
+
+# Uninstall Microsoft.Graph.Authentication
 $ModuleName = "Microsoft.Graph.Authentication"
 $Versions = Get-Module $ModuleName -ListAvailable
 Foreach ($Version in $Versions)
@@ -19,4 +35,6 @@ Foreach ($Version in $Versions)
     Write-Host "Uninstall-Module $ModuleName $ModuleVersion"
     Uninstall-Module $ModuleName -RequiredVersion $ModuleVersion
 }
+
+# Install the latest version (you may have to close all PowerShell before upgrading)
 Install-Module Microsoft.Graph
